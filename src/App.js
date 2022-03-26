cbdc639b42315d9c86070ae6018e273933c50ea8
dgabriel <dgabriel999@gmail.com>
2022-03-26 14:26:49 -0400

    emptied app initial things on the screen

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {SafeAreaView, View} from 'react-native';
+
+function App() {
+  return (
+    <SafeAreaView>
+    </SafeAreaView>
+  );
+};
+
+export default App;
