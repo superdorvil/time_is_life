3b9d04c38f1bfcfac3ccf347e56ecc43d55c0572
dgabriel <dgabriel999@gmail.com>
2022-03-26 14:49:19 -0400

    added app color schemes

---
+++ src/resources/colors.js
@@ -0,0 +1,67 @@
+export const orangeLight = 0;
+export const orangeDark = 1;
+export const blueLight = 2;
+export const blueDark = 3;
+
+// Orange Light
+export const orangeLightPrimary = '#F95700FF';
+export const orangeLightSecondary = '#FFFFFF';
+export const orangeLightTertiary = '#000000';
+export const orangeLightRemove = '#FF0000';
+export const orangeLightColorSchemeModal = '#212121';
+
+// Orange Dark
+export const orangeDarkPrimary = '#F95700FF';
+export const orangeDarkSecondary = '#000000';
+export const orangeDarkTertiary = '#FFFFFF';
+export const orangeDarkRemove = '#FF0000';
+export const orangeDarkColorSchemeModal = '#212121';
+
+// Blue Light
+export const blueLightPrimary = '#0DD3FF';
+export const blueLightSecondary = '#FFFFFF';
+export const blueLightTertiary = '#000000';
+export const blueLightRemove = '#FF0000';
+export const blueLightColorSchemeModal = '#212121';
+
+// Blue Dark
+export const blueDarkPrimary = '#0DD3FF';
+export const blueDarkSecondary = '#000000';
+export const blueDarkTertiary = '#FFFFFF';
+export const blueDarkRemove = '#FF0000';
+export const blueDarkColorSchemeModal = '#212121';
+
+export const primary = [
+  orangeLightPrimary,
+  orangeDarkPrimary,
+  blueLightPrimary,
+  blueDarkPrimary,
+];
+
+export const secondary = [
+  orangeLightSecondary,
+  orangeDarkSecondary,
+  blueLightSecondary,
+  blueDarkSecondary,
+];
+
+export const tertiary = [
+  orangeLightTertiary,
+  orangeDarkTertiary,
+  blueLightTertiary,
+  blueDarkTertiary,
+];
+
+export const remove = [
+  orangeLightRemove,
+  orangeDarkRemove,
+  blueLightRemove,
+  blueDarkRemove,
+];
+
+export const colorSchemeModal = [
+  orangeLightColorSchemeModal,
+  orangeDarkColorSchemeModal,
+  blueLightColorSchemeModal,
+  blueDarkColorSchemeModal,
+];
