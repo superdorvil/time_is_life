65bd109c967a0e12c836f8dfceaf93341eb944b5
dgabriel <dgabriel999@gmail.com>
2022-03-28 06:49:29 -0400

    added app states

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,6 @@
 import * as ICONS from './icons';
 import * as UTILS from './utils';
 import * as SCHEMAS from './schemas';
+import * as STATES from './states';
 
-export {ICONS, UTILS, SCHEMAS};
+export {ICONS, UTILS, SCHEMAS, STATES};
---
+++ src/constants/states.js
@@ -0,0 +1,10 @@
+export const task = 'task';
+export const timer = 'timer';
+export const goals = 'goals';
+
+export const daily = 'Daily';
+export const weekly = 'Weekly';
+export const monthly = 'Monthly';
+
+export const am = 'am';
+export const pm = 'pm';
