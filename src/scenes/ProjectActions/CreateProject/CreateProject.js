f575806470cc49f5abb75d503b3e957db8a5dfc3
dgabriel <dgabriel999@gmail.com>
2022-03-28 17:01:05 -0400

    starting the createproject page

--- src/scenes/ProjectActions/CreateProject/CreateProject.js
+++ src/scenes/ProjectActions/CreateProject/CreateProject.js
@@ -1,10 +1,73 @@
-import React from 'react';
-import {Text} from 'react-native';
+import React, {useState} from 'react';
+import {View} from 'react-native';
+import {Actions} from 'react-native-router-flux';
+import projectDB from '_data';
+import {ActionContainer} from '_components';
+import {Button, ProjectInput} from '_components';
+import {ICONS} from '_constants';
 
-function CreateProject() {
+function CreateProject({realm}) {
+  const actionScreenData = {
+    backArrowActive: true,
+    editButtonActive: false,
+    topRightButtonActive: false,
+    centerIconName: ICONS.checkmark,
+    actionDescription: 'Create New Project',
+  };
+  const [description, updateDescription] = useState();
+
+  const createProject = () => {
+    if (description !== '') {
+      projectDB.createProject({
+        realm,
+        description,
+      });
+
+      Actions.pop();
+    }
+  };
+
   return (
-    <Text>Create Project</Text>
+    <View style={containerStyle()}>
+      <ActionContainer
+        weeklyProgressActive={false}
+        weeklyProgressData={false}
+        actionScreenActive={true}
+        actionScreenData={actionScreenData}
+        actionNavBarActive={false}
+        actionNavBarData={false}
+        actionButtonActive={false}
+        actionButtonPressed={false}
+        listDataActive={false}
+        listData={false}
+        renderListItem={false}>
+        <ProjectInput
+          header="Project Name"
+          value={description}
+          onChangeText={updateDescription}
+          placeholder="enter project name ..."
+        />
+      </ActionContainer>
+      <View style={buttonStyle()}>
+        <Button
+          description="+ Add Project"
+          buttonPressed={createProject}
+        />
+      </View>
+    </View>
   );
 }
+
+const containerStyle = () => {
+  return {flex: 1};
+};
+
+const buttonStyle = () => {
+  return {
+    position: 'absolute',
+    left: 16,
+    bottom: 32,
+  };
+};
 
 export default CreateProject;
